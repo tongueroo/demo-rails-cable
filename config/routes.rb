b8ebac9a6418146b1474c89e8b8a691099f05de7
Tung Nguyen <tongueroo@gmail.com>
2026-03-31 22:36:10 +0000

    pwa service worker fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "service-worker.js", to: proc { [204, {"Content-Type" => "application/javascript"}, [""]] }
 
   root "home#show"
 end
