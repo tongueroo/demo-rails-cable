f3261f6ad828f53bac4a9f43784c5fab1171ccb6
Tung Nguyen <tongueroo@gmail.com>
2026-04-01 04:21:42 +0000

    set config.action_cable.url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
   config.action_cable.mount_path = nil
+  config.action_cable.url = ENV.fetch("ACTION_CABLE_URL", "/cable")
   config.action_cable.disable_request_forgery_protection = true
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!).
